449342ffe2a280ec4eb01f854f905a6430c168e8
chonamwoo <60260489+chonamwoo@users.noreply.github.com>
2020-12-14 14:46:32 +0900

    Add files via upload

--- combined/radarChart.js
+++ combined/radarChart.js
@@ -42,12 +42,12 @@ const RadarChart = function RadarChart(parent_selector, data, options) {
 	 w: 600,				//Width of the circle
 	 h: 600,				//Height of the circle
 	 margin: {top: 20, right: 20, bottom: 20, left: 20}, //The margins of the SVG
-	 levels: 3,				//How many levels or inner circles should there be drawn
+	 levels: 5.0,				//How many levels or inner circles should there be drawn
 	 maxValue: 0, 			//What is the value that the biggest circle will represent
 	 labelFactor: 1.25, 	//How much farther than the radius of the outer circle should the labels be placed
 	 wrapWidth: 60, 		//The number of pixels after which a label needs to be given a new line
 	 opacityArea: 0.35, 	//The opacity of the area of the blob
-	 dotRadius: 4, 			//The size of the colored circles of each blog
+	 dotRadius: 3, 			//The size of the colored circles of each blog
 	 opacityCircles: 0.1, 	//The opacity of the circles of each blob
 	 strokeWidth: 2, 		//The width of the stroke around each blob
 	 roundStrokes: false,	//If true the area and stroke will follow a round path (cardinal-closed)
@@ -144,9 +144,9 @@ const RadarChart = function RadarChart(parent_selector, data, options) {
 	   .attr("x", 4)
 	   .attr("y", d => -d * radius / cfg.levels)
 	   .attr("dy", "0.4em")
-	   .style("font-size", "10px")
+	   .style("font-size", "9px")
 	   .attr("fill", "#737373")
-	   .text(d => Format(maxValue * d / cfg.levels) + cfg.unit);
+	   .text(d => Format((100).toFixed(maxValue) * d / cfg.levels) + " %");
 
 	/////////////////////////////////////////////////////////
 	//////////////////// Draw the axes //////////////////////
